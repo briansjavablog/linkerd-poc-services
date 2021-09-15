95f4e6639ab90e2ee2f4b864a98148e78d4d34b2
unknown <hannawaybrian@googlemail.com>
2021-09-15 09:39:07 +0100

    k8s manifest includes Deployments and Services for microservices

--- scripts/buildAndPushImages.sh
+++ scripts/buildAndPushImages.sh
@@ -4,19 +4,19 @@ cd ..
 cd CurrentAccountService
 mvn clean package
 docker image build -t briansjavablog/current-account-service:latest .
-#docker push briansjavablog/current-account-service:latest
+docker push briansjavablog/current-account-service:latest
 cd ..
 cd SavingsAccountService
 mvn clean package
 docker image build -t briansjavablog/savings-account-service:latest .
-#docker push briansdevblog/saving-account-service:latest
+docker push briansjavablog/savings-account-service:latest
 cd ..
 cd CreditCardService
 mvn clean package
 docker image build -t briansjavablog/credit-card-service:latest .
-#docker push briansdevblog/credit-card-service:latest
+docker push briansjavablog/credit-card-service:latest
 cd ..
 cd BankingService
 mvn clean package
 docker image build -t briansjavablog/banking-service:latest .
-#docker push briansdevblog/banking-service:latest
+docker push briansjavablog/banking-service:latest
--- scripts/createAKSClusterAndDeploy.sh
+++ scripts/createAKSClusterAndDeploy.sh
@@ -7,4 +7,5 @@ az group create --location $LOCATION --resource-group $RESOURCE_GROUP
 az aks create --resource-group=$RESOURCE_GROUP --name=$CLUSTER_NAME --node-count=1 --generate-ssh-keys --node-vm-size Standard_B2s --enable-managed-identity
 az aks get-credentials --overwrite-existing --name $CLUSTER_NAME --resource-group $RESOURCE_GROUP
 cd ../Kubernetes
-kubectl apply -f linkerd-poc-cluster.yml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
